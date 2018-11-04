582c737e01cfe407aed3f32e0223f5561256aa68
Prem Agarwal <prem.m.agarwal@gmail.com>
2018-11-03 21:14:09 -0700

    Add files via upload

---
+++ CCExtractorDevelopment.py
@@ -0,0 +1,7 @@
+x = "GCI is great"
+for i in range(10): ## Repeat 10 times
+    print x 
+    
+name = raw_input("What is your name: ")
+print "Hello " + name+", please to meet you!"
+print "Hello " + name + ", please to meet you! Did you know that your name backwards is "+name[::-1]+"?"
